3812be2964e08f8eabafaa0aa9602d5dadf7f4a4
Keith Balante <kbalante@cisco.com>
2022-03-05 19:20:36 -0700

    add stronger password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
 
+  validate :password_complexity
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,70}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be 6-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
