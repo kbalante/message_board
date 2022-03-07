b18d7f925b0b0b8d11dea30b48d3d4401bfd65fb
Keith Balante <kbalante@cisco.com>
2022-03-07 12:14:32 -0700

    remove comments index from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  resources :comments, :except => [:show]
+  resources :comments, :except => [:show, :index]
 
   devise_for :users
 
