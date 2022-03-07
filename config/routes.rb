a2e1a2f220f8c173eab1b7c8889d563d08e9ccb5
Keith Balante <kbalante@cisco.com>
2022-03-07 10:25:46 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  resources :comments
-  get '/posts', to: 'posts#index'
+  resources :comments, :except => [:show]
 
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: "posts#index"
 end
